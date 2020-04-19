b59b4c5ea1431246a76ce98f757c97ad618656de
Brian Ardiles <bardiles@aivo.co>
2020-04-19 14:04:04 -0300

    add dark theme automagic  and switch

---
+++ js/dark.js
@@ -0,0 +1,31 @@
+window.onload = () => {
+    const lightModeButton = document.getElementById("light-mode")
+    const darkModeButton = document.getElementById("dark-mode")
+
+    lightModeButton.addEventListener('click',()  => {
+        setMode('light', lightModeButton, darkModeButton)
+    });
+
+    darkModeButton.addEventListener('click',()  => {
+        setMode('dark', lightModeButton, darkModeButton)
+    });
+
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        setMode('dark', lightModeButton, darkModeButton)
+    } else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches){
+        setMode('light', lightModeButton, darkModeButton)
+    }
+}
+
+
+const setMode = (mode, lightModeButton, darkModeButton) => {
+    if (mode === 'light') {
+        document.body.classList.remove("dark")
+        lightModeButton.classList.add("active-mode")
+        darkModeButton.classList.remove("active-mode")
+    } else if (mode === 'dark') {
+        document.body.classList.add("dark")
+        lightModeButton.classList.remove("active-mode")
+        darkModeButton.classList.add("active-mode")
+    }
+}
